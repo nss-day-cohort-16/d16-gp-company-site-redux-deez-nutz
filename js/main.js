9800ff614904e35099956528ab49363a3f8d42ea
Rico <richardthomaswhitfield@gmail.com>
2016-10-13 09:50:05 -0500

    included js and products page w/ additional styling

--- js/main.js
+++ js/main.js
@@ -1,94 +1,46 @@
-var nutArray = [
-{
-	"Strain": "Walnut",
-	"Appearance": "Rustic Brown",
-	"Flavor": "Mildly Nutty",
-	"Accompanies": "White Wine",
-	"Occasions": "Labor Day",
-	"Price": "$8.99 per Lb.",
-	"Image": "images/walnut.jpg"
-},
-{
-	"Strain": "Cashew",
-	"Appearance": "Golden Brown",
-	"Flavor": "Earthy Hues",
-	"Accompanies": "Coffee",
-	"Occasions": "Despair",
-	"Price": "$15.99 per Lb.",
-	"Image": "images/cashews.jpg"
-},
+function Nut(strain, appearance, flavor, accompanies, occasions, price, image) {
+	this.strain = strain;
+	this.appearance = appearance;
+	this.flavor = flavor;
+	this.accompanies = accompanies;
+	this.occasions = occasions;
+	this.price = price;
+	this.image = image;
+}
 
-{
-	"Strain": "Pistachios",
-	"Appearance": "Greeny Yellow",
-	"Flavor": "As expected",
-	"Accompanies": "Sharp Jack, Vermont",
-	"Occasions": "Grandma's Disco Party",
-	"Price": "$11.99 per Lb.",
-	"Image": "images/pistachios.jpg"
-},
+var walnut = new Nut ("Walnut", "Rustic Brown", "Mildly Nutty", "White Wine", "Labor Day", "8.99 per lb.", "images/walnut.jpg");
 
-{
-	"Strain": "Coconut",
-	"Appearance": "Furry",
-	"Flavor": "Sun Tan Lotion",
-	"Accompanies": "Lime in the",
-	"Occasions": "Luau's",
-	"Price": "$1.99 per Lb.",
-	"Image": "images/coconuts.jpg"
-},
+var cashew = new Nut ("Cashew", "Golden Brown", "Earthy Hues", "Coffee", "Despair", "$15.99 per Lb.", "images/cashews.jpg");
 
-{
-	"Strain": "Wing Nut",
-	"Appearance": "Metallica",
-	"Flavor": "Iron",
-	"Accompanies": "Hardware",
-	"Occasions": "Hard Work",
-	"Price": "$.99 per Lb.",
-	"Image": "images/wingNut.JPG"
-},
+var pistachio = new Nut ("Pistachios", "Greeny Yellow", "As expected", "Sharp Jack, Vermont", "Grandma's Disco Party", "$11.99 per Lb.","images/pistachios.jpg");
 
-{
-	"Strain": "Tee Nut",
-	"Appearance": "T Like",
-	"Flavor": "Sweet",
-	"Accompanies": "Wood",
-	"Occasions": "Carpentry",
-	"Price": "$.99 per Lb.",
-	"Image": "images/teeNut.jpg"
-},
+var coconuts = new Nut ("Coconut", "Furry", "Sun Tan Lotion", "Lime in the", "Luau's", "$1.99 per Lb.", "images/coconuts.jpg");
 
-];
+var wingNut = new Nut ("Wing Nut", "Metallica","Iron", "Hardware","Hard Work", "$.99 per Lb.", "images/wingNut.JPG");
 
+var teeNut = new Nut ("Tee Nut","T Like","Sweet","Wood","Carpentry","$.99 per Lb.", "images/teeNut.jpg");
 
+var nutArray = [];
+nutArray.push(walnut, cashew, pistachio, coconuts, wingNut, teeNut);
 
+// console.log(nutArray);
 
 for (var i = 0; i < nutArray.length; i++) {
 	var currentNutArray = nutArray[i];
-	var strain = "<h2 class='strain'>" + "Strain: "+ currentNutArray.Strain + "</h2>";
-	var image = "<img class='nutPics' src='" + currentNutArray.Image + "'>";
-	var appearance = "<h4 id='appearance'>" + "Appearance: " + currentNutArray.Appearance + "</h4>"; 
-	var flavor = "<p>" + "Flavor: " + currentNutArray.Flavor + "</p>";
-	var accompanies = "<p>" + "Accompanies: "+ currentNutArray.Accompanies + "</p>";
-	var occassions = "<p>" + "Occasions: " + currentNutArray.Occasions + "</p>";
-	var price = "<h5>" + "Price: " + currentNutArray.Price + "</h5>";
-
+	var nutString = "<h2 class='strain'>" + "Strain: "+ currentNutArray.strain + "</h2>" + "<img class='nutPics' src='" + currentNutArray.image + "'>" + "<h4 id='appearance'>" + "Appearance: " + currentNutArray.Appearance + "</h4>" + "<p>" + "Flavor: " + currentNutArray.flavor + "</p>" +"<p>" + "Accompanies: "+ currentNutArray.accompanies + "</p>" + "<p>" + "Occasions: " + currentNutArray.occasions + "</p>" + "<h5>" + "Price: " + currentNutArray.price + "</h5>";
 
 	var outputDiv = document.getElementById("productsOutput");
-	outputDiv.innerHTML += "<div class='card'>" + image + strain + appearance + flavor + accompanies + occassions + price + "</div>";
+	outputDiv.innerHTML += "<div class='card'>"+ nutString + "</div>";
 }
+ 
+ 
+
 	
 	
 
 
 
 
-
-
-
-
-
-// console.log("output", productsOutput);
 
 
 
